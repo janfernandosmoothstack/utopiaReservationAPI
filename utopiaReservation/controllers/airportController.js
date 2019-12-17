c77cbc87ecf6ee73cbb354068daa6358e44502c0
janfernandosmoothstack <janet.fernando@smoothstack.com>
2019-12-17 16:32:13 -0500

    airport

--- utopiaReservation/controllers/airportController.js
+++ utopiaReservation/controllers/airportController.js
@@ -10,4 +10,21 @@ routes.get('/airports',function(request ,response){
     });
 });
 
+routes.get('/airports/departureAirport/:departureAirport/arrivalAirport/:arrivalAirport', (request, response) => {
+  var departureAirport = request.params.departureAirport;
+  var arrivalAirport = request.params.arrivalAirport;
+
+  airportDao.getAirport(departureAirport, arrivalAirport, (err, airportRes) =>{
+      if(err) throw error;
+
+      if (airportRes.length == 0) {
+          const result = {message: "Record not found"};
+          response.status(404).send(result);
+      } else {
+          response.setHeader('Content-Type', 'application/json');
+          response.status(200).send(airportRes);
+      }
+  });
+});
+
 module.exports = routes;
